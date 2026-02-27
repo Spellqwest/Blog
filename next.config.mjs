7c5c46c6aead84db2b38a982141282810419c00f
EinsEli <57264430+EinsEli@users.noreply.github.com>
2026-02-27 13:10:16 +0100

    Add deployment to GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,17 @@ const withNextra = nextra({
   defaultShowCopyCode: true,
 });
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const basePath = isGithubActions && repoName ? `/${repoName}` : undefined;
+
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/resources",
-        permanent: true,
-      },
-    ];
+  output: isGithubActions ? "export" : undefined,
+  trailingSlash: isGithubActions,
+  images: {
+    unoptimized: true,
   },
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
 });
