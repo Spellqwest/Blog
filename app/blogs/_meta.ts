bbb74f00ecf32d38178731a79b248ae8e3ea91ea
EinsEli <57264430+EinsEli@users.noreply.github.com>
2026-03-26 19:11:26 +0100

    Added analytics to tasks this week

--- app/blogs/_meta.ts
+++ app/blogs/_meta.ts
@@ -2,4 +2,5 @@ export default {
 	"20260305_blog-1": "First beginnings 🎉",
 	"20260313_blog-2": "Second time's the charm 👀",
 	"20260319_blog-3": "Third time's even better 🏆",
+	"20260326_blog-4": "On all fours 📊",
 };
